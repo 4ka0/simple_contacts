c72b5e80093335038bcc931c22fa048d55aaa024
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-02-25 14:56:22 +0900

    Model test added

--- contacts/tests/test_models.py
+++ contacts/tests/test_models.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 from contacts.models import Contact
+
+from freezegun import freeze_time
 
 
 class ContactModelTest(TestCase):
@@ -23,49 +26,166 @@ class ContactModelTest(TestCase):
             last_modified_on="testuser@email.com",
         )
 
-    def test_username_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field("username").verbose_name
-        self.assertEqual(field_label, "username")
+    # Test field labels
 
     def test_first_name_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field("first_name").verbose_name
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("first_name").verbose_name
         self.assertEqual(field_label, "first name")
 
     def test_last_name_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field("last_name").verbose_name
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("last_name").verbose_name
         self.assertEqual(field_label, "last name")
 
-    def test_position_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field("position").verbose_name
-        self.assertEqual(field_label, "position")
+    def test_nickname_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("nickname").verbose_name
+        self.assertEqual(field_label, "nickname")
 
-    def test_email_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field("email").verbose_name
+    def test_postal_address_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("postal_address").verbose_name
+        self.assertEqual(field_label, "postal address")
+
+    def test_phone_number_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("phone_number").verbose_name
+        self.assertEqual(field_label, "phone number")
+
+    def test_email_address_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("email_address").verbose_name
         self.assertEqual(field_label, "email address")
 
-    def test_position_max_length(self):
-        user = CustomUser.objects.get(id=1)
-        max_length = user._meta.get_field("last_name").max_length
-        self.assertEqual(max_length, 150)
+    def test_linkedin_url_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("linkedin_url").verbose_name
+        self.assertEqual(field_label, "linkedin url")
+
+    def test_twitter_url_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("twitter_url").verbose_name
+        self.assertEqual(field_label, "twitter url")
+
+    def test_github_url_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("github_url").verbose_name
+        self.assertEqual(field_label, "github url")
+
+    def test_personal_website_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("personal_website").verbose_name
+        self.assertEqual(field_label, "personal website")
+
+    def test_created_on_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("created_on").verbose_name
+        self.assertEqual(field_label, "created on")
+
+    def test_personal_website_label(self):
+        contact = Contact.objects.get(id=1)
+        field_label = contact._meta.get_field("last_modified_on").verbose_name
+        self.assertEqual(field_label, "last modified on")
+
+    # Test field max_lengths
+
+    def test_first_name_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("first_name").max_length
+        self.assertEqual(max_length, 50)
+
+    def test_last_name_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("last_name").max_length
+        self.assertEqual(max_length, 50)
+
+    def test_nickname_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("name_name").max_length
+        self.assertEqual(max_length, 50)
+
+    def test_postal_address_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("postal_address").max_length
+        self.assertEqual(max_length, 200)
+
+    def test_phone_number_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("phone_number").max_length
+        self.assertEqual(max_length, 50)
+
+    def test_email_address_max_length(self):
+        contact = Contact.objects.get(id=1)
+        max_length = contact._meta.get_field("email_address").max_length
+        self.assertEqual(max_length, 200)
+
+    # Test object created correctly
 
     def test_object_instatiation(self):
-        user = CustomUser.objects.get(id=1)
+        contact = Contact.objects.get(id=1)
+
         self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(user.username, "testuser")
-        self.assertNotEqual(user.username, "")
-        self.assertEqual(user.email, "testuser@email.com")
-        self.assertNotEqual(user.email, "")
-        self.assertEqual(user.first_name, "Test")
-        self.assertNotEqual(user.first_name, "")
-        self.assertEqual(user.last_name, "User")
-        self.assertNotEqual(user.last_name, "")
-        self.assertEqual(user.position, "Tester")
-        self.assertNotEqual(user.position, "")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
+        self.assertNotEqual(get_user_model().objects.all().count(), 0)
+
+        self.assertEqual(contact.first_name, "Test")
+        self.assertNotEqual(contact.first_name, "")
+
+        self.assertEqual(contact.last_name, "User")
+        self.assertNotEqual(contact.last_name, "")
+
+        self.assertEqual(contact.nickname, "Tester")
+        self.assertNotEqual(contact.nickname, "")
+
+        self.assertEqual(contact.postal_address, "23 Test Avenue, Test Road, Testville, Testshire, TS3 3TY, UK")
+        self.assertNotEqual(contact.last_name, "")
+
+        self.assertEqual(contact.phone_number, "090-8745-2365")
+        self.assertNotEqual(contact.phone_number, "")
+
+        self.assertEqual(contact.email_address, "testuser@email.com")
+        self.assertNotEqual(contact.email_address, "")
+
+        self.assertEqual(contact.linkedin_url, "https://www.linkedin.com/testuser")
+        self.assertNotEqual(contact.linkedin_url, "")
+
+        self.assertEqual(contact.twitter_url, "https://www.twitter.com/testuser")
+        self.assertNotEqual(contact.twitter_url, "")
+
+        self.assertEqual(contact.github_url, "https://www.github.com/testuser")
+        self.assertNotEqual(contact.github_url, "")
+
+        self.assertEqual(contact.personal_website, "https://www.testuser.com")
+        self.assertNotEqual(contact.personal_website, "")
+
+    @freeze_time("2020-02-25 14:38:00")
+    def test_created_on_date(self):
+        contact = Contact.objects.get(id=1)
+        self.assertEqual(
+            datetime.strftime(contact.created_on, "%Y-%m-%d %H:%M:%S")
+            "2020-02-25 14:38:00"
+        )
+
+    def test_last_modified_on_date_after_creation(self):
+        contact = Contact.objects.get(id=1)
+        self.assertEqual(contact.last_modified_on, None)  # Should be None when object created
+
+    @freeze_time("2020-02-25 14:38:00")
+    def test_last_modified_on_date_after_modification(self):
+        contact = Contact.objects.get(id=1)
+        contact.last_modified_on = timezone.now()
+        self.assertEqual(
+            datetime.strftime(contact.last_modified_on, "%Y-%m-%d %H:%M:%S")
+            "2020-02-25 14:38:00"
+        )
+
+    # Test class methods
+
+    def test_get_absolute_url(self):
+        contact = Contact.objects.get(id=1)
+        self.assertEqual(contact.get_absolute_url(), '/1')
+
+    def test_str_representation(self):
+        contact = Contact.objects.get(id=1)
+        expected_object_name = f'{contact.first_name} {contact.last_name}'
+        self.assertEqual(expected_object_name, str(contact))
